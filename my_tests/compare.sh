59b7fbe2b0bd0045f08eec1f9126133c9c2c4b6e
JiaweiZhuang <jiaweizhuang@g.harvard.edu>
2017-08-24 10:13:07 -0400

    Tests for comparing different languages
    Signed-off-by: JiaweiZhuang <jiaweizhuang@g.harvard.edu>

---
+++ my_tests/compare.sh
@@ -0,0 +1,9 @@
+echo '===================='
+echo 'C'
+time ./saprc_c/saprc_c.exe > /dev/null
+echo '===================='
+echo 'F77'
+time ./saprc_f77/saprc_f77.exe > /dev/null
+echo '===================='
+echo 'F90'
+time ./saprc_f90/saprc_f90.exe > /dev/null
