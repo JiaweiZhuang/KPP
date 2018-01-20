86f6e6a0cf32d3fe1efc54eb24c1bf5469fe75e0
JiaweiZhuang <jiaweizhuang@g.harvard.edu>
2018-01-20 18:59:39 -0500

    Use GEOS-Chem solver option

--- my_tests/GEOSChem/gckpp_Main.f90
+++ my_tests/GEOSChem/gckpp_Main.f90
@@ -7,9 +7,9 @@ USE gckpp_Precision, ONLY: sp, dp
 IMPLICIT NONE
 
 REAL(dp) :: TIN, TOUT ! DT is already defined in gckpp_Global
-!INTEGER                :: ICNTRL(20), ISTATUS(20)
-!REAL(dp)               :: RCNTRL(20 ), RSTATE(20)
-
+INTEGER  :: ICNTRL(20), ISTATUS(20), IERR
+REAL(dp) :: RCNTRL(20), RSTATE(20)
+     
 !%%%%% TIMESTEPS %%%%%
 DT        = 20d0 * 60d0 ! in seconds
 TIN       = 0d0
@@ -20,28 +20,35 @@ TOUT      = TIN + DT
 ATOL      = 1e-2_dp    
 RTOL      = 0.5e-2_dp 
 
-! !%%%%% SOLVER OPTIONS %%%%%
-! ICNTRL    = 0 ! Zero all slots of IC
-! ICNTRL(1) = 1	
-! ICNTRL(2) = 0	
-! ICNTRL(3) = 4
-! ICNTRL(7) = 1
-! RCNTRL    = 0.0_fp ! Zero all slots of RCNTRL
-! RCNTRL(3) = HSAVE_KPP(I,J,L)
+!%%%%% SOLVER OPTIONS %%%%%
+! taken from flexchem_mod.F90
+ICNTRL    = 0 ! Zero all slots of IC
+ICNTRL(1) = 1	
+ICNTRL(2) = 0	
+ICNTRL(3) = 4
+ICNTRL(7) = 1
+RCNTRL    = 0.0_dp ! Zero all slots of RCNTRL
+!! RCNTRL(3) = HSAVE_KPP(I,J,L) ! Only do one time step. Don't have this.
 
-CALL Initialize()
+CALL Initialize() ! don't seem to do anything
 
+! TODO: use real data
 C = 1.0
 RCONST = 1.0
 
 print*, 'C: ', C(1: 10)
 print*, 'RCONST: ', RCONST(1: 10)
+print*, 'RCNTRL: ', RCNTRL 
 
 ! Call the KPP integrator
-CALL Integrate(TIN,    TOUT)
+CALL Integrate( TIN,    TOUT,    ICNTRL,      &
+                RCNTRL, ISTATUS, RSTATE, IERR )
 
 print*, 'after:'
 print*, 'C: ', C(1: 10)
 print*, 'RCONST: ', RCONST(1: 10)
+print*, 'RSTATE: ', RSTATE
+print*, 'IERR: ', IERR
+
 
 END PROGRAM gckpp_Driver
