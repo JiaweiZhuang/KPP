f78cc5d842549328e61dbfe64944df2af495be6c
JiaweiZhuang <jiaweizhuang@g.harvard.edu>
2018-01-20 15:32:16 -0500

    Immediately after running "kpp ros.kpp"

---
+++ my_tests/test_f2py/ros_Model.f90
@@ -0,0 +1,22 @@
+MODULE ros_Model
+
+!~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+!  Completely defines the model ros
+!    by using all the associated modules
+!~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+  USE ros_Precision
+  USE ros_Parameters
+  USE ros_Global
+  USE ros_Function
+  USE ros_Integrator
+  USE ros_Rates
+  USE ros_Jacobian
+  USE ros_Hessian
+  USE ros_Stoichiom
+  USE ros_LinearAlgebra
+  USE ros_Monitor
+  USE ros_Util
+
+END MODULE ros_Model
+
