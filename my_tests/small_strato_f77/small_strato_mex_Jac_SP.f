a45307400447e7762977de661e0cc36846dfb4d0
JiaweiZhuang <jiaweizhuang@g.harvard.edu>
2018-02-23 22:03:35 -0500

    generate F77 code

---
+++ my_tests/small_strato_f77/small_strato_mex_Jac_SP.f
@@ -0,0 +1,43 @@
+
+      SUBROUTINE mexFunction( nlhs, plhs, nrhs, prhs )
+C~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+C               Matlab Gateway for the Sparse Jacobian Function Jac_SP
+C~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+      INTEGER nlhs, nrhs
+      INTEGER plhs(*), prhs(*)
+      INTEGER mxGetPr, mxCreateFull, mxGetM, mxgetN
+      INTEGER VPtr, FPtr, RPtr, JVSPtr
+      REAL*8 V(5), F(2), RCT(10)
+      REAL*8 JVS(19)
+
+C Check for the right number of input arguments
+      IF ( nrhs .ne. 3 ) THEN
+         CALL mexErrMsgTxt('Jac_SP requires 3 input vectors: 
+     &V(5), F(2), RCT(10)')
+      END IF 
+C Check for the right number of output arguments
+      IF ( nlhs .ne. 1 ) THEN
+         CALL mexErrMsgTxt('Jac_SP requires 1 output vector: 
+     &JVS(19)')
+      END IF 
+
+      plhs(1) = mxCreateDoubleMatrix(19,1,0)
+
+      VPtr = mxGetPr(prhs(1))
+      CALL mxCopyPtrToReal8(VPtr,V,5)
+      
+      FPtr = mxGetPr(prhs(2))
+      CALL mxCopyPtrToReal8(FPtr,F,2)
+      
+      RPtr = mxGetPr(prhs(3))
+      CALL mxCopyPtrToReal8(RPtr,RCT,10)
+
+      JVSPtr = mxGetPr(plhs(1))
+
+      CALL Jac_SP( V, F, RCT, JVS )
+
+      CALL mxCopyReal8ToPtr(JVS, JVSPtr, 19)
+
+      RETURN 
+      END
