e9e4021eb8cfaf39128775d1c7f4434240d88007
JiaweiZhuang <jiaweizhuang@g.harvard.edu>
2018-02-28 10:57:48 -0500

    Don't time C

--- my_tests/compare_languages/compare.sh
+++ my_tests/compare_languages/compare.sh
@@ -1,6 +1,6 @@
-echo '===================='
-echo 'C'
-time ./saprc_c/saprc_c.exe > /dev/null
+#echo '===================='
+#echo 'C'
+#time ./saprc_c/saprc_c.exe > /dev/null
 echo '===================='
 echo 'F77'
 time ./saprc_f77/saprc_f77.exe > /dev/null
